0fb3c4a91f6e2968140dd7991ff77426ff2a5bc0
Mathieu Rousse <mathieu@rousse.me>
2017-03-22 17:47:22 +0100

    Ajout Listener sur NetworkTopology.Topo.Node

--- impl/src/main/java/com/orange/cloudsec/impl/CloudSecProvider.java
+++ impl/src/main/java/com/orange/cloudsec/impl/CloudSecProvider.java
@@ -7,22 +7,31 @@
  */
 package com.orange.cloudsec.impl;
 
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.controller.md.sal.binding.api.*;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CloudSecProvider {
+import javax.annotation.Nonnull;
+import java.util.Collection;
+
+public class CloudSecProvider implements DataTreeChangeListener<Node>, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(CloudSecProvider.class);
 
     private final DataBroker dataBroker;
-    private final NotificationProviderService notificationService;
+    private final NotificationPublishService notificationService;
     private final SalFlowService salFlowService;
 
     public CloudSecProvider(final DataBroker dataBroker,
-		    final NotificationProviderService notificationService,
+		    final NotificationPublishService notificationService,
 		    final SalFlowService salFlowService) {
 
         this.dataBroker = dataBroker;
@@ -34,6 +43,17 @@ public class CloudSecProvider {
      * Method called when the blueprint container is created.
      */
     public void init() {
+        InstanceIdentifier<Node> path = InstanceIdentifier.create(NetworkTopology.class)
+                .child(Topology.class, new TopologyKey(new TopologyId("flow:1")))
+                .child(Node.class);
+        final DataTreeIdentifier<Node> treeId = new DataTreeIdentifier<Node>(LogicalDatastoreType.OPERATIONAL, path);
+        try {
+            LOG.info("Cloud : Registering on {}", treeId);
+            dataBroker.registerDataTreeChangeListener(treeId, this);
+        } catch (final Exception e){
+            LOG.warn("Cloud : Registration failed :/");
+        }
+
         LOG.info("CloudSecProvider Session Initiated");
     }
 
@@ -42,5 +62,10 @@ public class CloudSecProvider {
      */
     public void close() {
         LOG.info("CloudSecProvider Closed");
+    }
+
+    @Override
+    public void onDataTreeChanged(Collection<DataTreeModification<Node>> collection) {
+        LOG.info("Cloud : Data Changed :)");
     }
 }
