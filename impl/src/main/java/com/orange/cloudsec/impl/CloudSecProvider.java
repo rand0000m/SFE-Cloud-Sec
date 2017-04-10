6aaeb1c3b2673595298a77578f476ce539ce9579
Mathieu Rousse <mathieu@rousse.me>
2017-04-10 11:32:56 +0200

    Suppression traitement paquets, ajout configuration SF & SFF

--- impl/src/main/java/com/orange/cloudsec/impl/CloudSecProvider.java
+++ impl/src/main/java/com/orange/cloudsec/impl/CloudSecProvider.java
@@ -7,35 +7,30 @@
  */
 package com.orange.cloudsec.impl;
 
-import org.jnetpcap.packet.JMemoryPacket;
-import org.jnetpcap.packet.JPacket;
-import org.jnetpcap.protocol.JProtocol;
-import org.jnetpcap.protocol.lan.Ethernet;
 import org.opendaylight.controller.md.sal.binding.api.*;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.*;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.*;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cloud.sec.rev150105.ServiceFunctionForwarderRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cloud.sec.rev150105.ServiceFunctionRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cloud.sec.rev150105.service.function.forwarder.registry.ServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cloud.sec.rev150105.service.function.registry.ServiceFunction;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class CloudSecProvider implements DataTreeChangeListener<Node>, AutoCloseable, PacketProcessingListener {
+public class CloudSecProvider implements DataTreeChangeListener, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(CloudSecProvider.class);
 
     private final DataBroker dataBroker;
     private final NotificationService notificationService;
     private final SalFlowService salFlowService;
-    private PacketProcessingService packetProcessingService;
     private final RpcProviderRegistry rpcProviderRegistry;
 
     private List<Switch> switches;
@@ -54,12 +49,17 @@ public class CloudSecProvider implements DataTreeChangeListener<Node>, AutoClose
     }
 
     private void registerInventoryChangeListener(){
-        InstanceIdentifier<Node> path = InstanceIdentifier.create(Nodes.class)
-                .child(Node.class);
-        final DataTreeIdentifier<Node> treeId = new DataTreeIdentifier<>(LogicalDatastoreType.OPERATIONAL, path);
+        InstanceIdentifier<ServiceFunction> pathSF = InstanceIdentifier.create(ServiceFunctionRegistry.class)
+                .child(ServiceFunction.class);
+        InstanceIdentifier<ServiceFunctionForwarder> pathSFF = InstanceIdentifier.create(ServiceFunctionForwarderRegistry.class)
+                .child(ServiceFunctionForwarder.class);
+        final DataTreeIdentifier<ServiceFunction> idSFTree = new DataTreeIdentifier<>(LogicalDatastoreType.CONFIGURATION, pathSF);
+        final DataTreeIdentifier<ServiceFunctionForwarder> idSFFTree = new DataTreeIdentifier<>(LogicalDatastoreType.CONFIGURATION, pathSFF);
         try {
-            LOG.info("Cloud : Registering on {}", treeId);
-            dataBroker.registerDataTreeChangeListener(treeId, this);
+            LOG.info("Cloud : Registering on {}", idSFTree);
+            dataBroker.registerDataTreeChangeListener(idSFTree, this);
+            LOG.info("Cloud : Registering on {}", idSFFTree);
+            dataBroker.registerDataTreeChangeListener(idSFFTree, this);
         } catch (final Exception e){
             LOG.warn("Cloud : Registration failed :/");
         }
@@ -70,8 +70,6 @@ public class CloudSecProvider implements DataTreeChangeListener<Node>, AutoClose
      */
     public void init() {
         registerInventoryChangeListener();
-        this.packetProcessingService = rpcProviderRegistry.getRpcService(PacketProcessingService.class);
-        notificationService.registerNotificationListener(this);
         LOG.info("CloudSecProvider Session Initiated");
     }
 
@@ -83,92 +81,7 @@ public class CloudSecProvider implements DataTreeChangeListener<Node>, AutoClose
     }
 
     @Override
-    public void onDataTreeChanged(Collection<DataTreeModification<Node>> collection) {
-        // Parcours de l'ensemble des modifications (create/update/delete)
-        for(DataTreeModification<Node> mod : collection){
-            DataObjectModification<Node> rootNode = mod.getRootNode();
-
-            if(rootNode.getModificationType() == DataObjectModification.ModificationType.WRITE){
-                if(rootNode.getDataBefore() == null){ // Si le node est créé
-                    Switch newNode = new Switch(mod.getRootPath().getRootIdentifier(), dataBroker);
-                    switches.add(newNode);
-                    LOG.info("Node {} created", mod.getRootPath().getRootIdentifier());
-                }
-            }else if(rootNode.getModificationType() == DataObjectModification.ModificationType.DELETE){
-                // Lors de la suppression d'un node, retrait du datastore configuration
-                Switch toDelete = Switch.getSwitchByIid(switches, mod.getRootPath().getRootIdentifier());
-                if(toDelete != null)
-                    toDelete.killMe();
-                LOG.info("Node {} has been deleted", mod.getRootPath().getRootIdentifier());
-            }else{
-                // Cas ne devant pas se présenter
-                LOG.warn("Something changed, but I'm not quite sure what...");
-            }
-        }
-    }
-
-
-    @Override
-    public void onPacketReceived(PacketReceived packetReceived) {
-        // Identification du Switch recevant le paquet
-        NodeKey nodeKey = null;
-        Iterable<InstanceIdentifier.PathArgument> pathArgs = packetReceived.getIngress().getValue().getPathArguments();
-        for(InstanceIdentifier.PathArgument pathArgument: pathArgs){
-            if(pathArgument instanceof InstanceIdentifier.IdentifiableItem){
-                if(((InstanceIdentifier.IdentifiableItem) pathArgument).getKey() instanceof NodeKey)
-                    nodeKey = (NodeKey) ((InstanceIdentifier.IdentifiableItem) pathArgument).getKey();
-            }
-        }
-        InstanceIdentifier<Node> node =
-                InstanceIdentifier.create(Nodes.class)
-                .child(Node.class, nodeKey);
-
-        Switch sw = Switch.getSwitchByIid(switches, node);
-
-        // Parsing du paquet
-        byte[] payload = packetReceived.getPayload();
-        JPacket packet = new JMemoryPacket(JProtocol.ETHERNET_ID, payload);
-
-        //Ignore LLDP packets, or you will be in big trouble
-        Ethernet eth = new Ethernet();
-        if (!packet.hasHeader(JProtocol.ETHERNET_ID) || packet.getHeader(eth).type() == 0x88cc) {
-            return;
-        }
-
-        // Emission du paquet sur tous les ports sauf le port entrant
-        NodeConnectorRef egress;
-        for(NodeConnector nc : sw.connectors){
-            if(packetReceived.getIngress() != nc){
-                egress = new NodeConnectorRef(
-                        node.child(NodeConnector.class, nc.getKey()));
-                TransmitPacketInput input = new TransmitPacketInputBuilder()
-                        .setPayload(packetReceived.getPayload())
-                        .setNode(new NodeRef(node))
-                        .setIngress(packetReceived.getIngress())
-                        .setEgress(egress)
-                        .build();
-                packetProcessingService.transmitPacket(input);
-            }
-        }
-
-        LOG.info("Transmitted packet from {} to {} - EtherType {} - Ethernet : {} - IP4 : {} - IP6 : {}",
-                bytesToHex(packet.getHeader(eth).source()),
-                bytesToHex(packet.getHeader(eth).destination()),
-                Integer.toHexString(packet.getHeader(eth).type() & 0xffff),
-                packet.hasHeader(JProtocol.ETHERNET_ID),
-                packet.hasHeader(JProtocol.IP4_ID),
-                packet.hasHeader(JProtocol.IP6_ID));
-    }
-
-    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-    @org.jetbrains.annotations.NotNull
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
+    public void onDataTreeChanged(@Nonnull Collection collection) {
+        LOG.warn("A SERVICE FUNCTION CHANGED !");
     }
 }
