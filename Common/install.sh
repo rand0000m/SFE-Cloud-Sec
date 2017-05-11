3b4bf9465de6d503eb523cbd572f680620403993
Mathieu Rousse <mathieu@rousse.me>
2017-05-11 16:55:46 +0200

    Reorganisation des dossiers + Documentation en cours

---
+++ Common/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+wget "https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.5.2-Boron-SR2/distribution-karaf-0.5.2-Boron-SR2.zip"
+unzip distribution-karaf-0.5.2-Boron-SR2.zip
+
+echo "deb http://ftp.fr.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
+
+echo 'JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"' > /etc/environment
+echo '_JAVA_OPTIONS="-Djava.net.preferIPv4Stack=true"' >> /etc/environment
+echo 'MAVEN_OPTS="-Xmx1024m"' >> /etc/environment
+
+apt update
+apt install -y -t jessie-backports maven git vim openjdk-8-jdk openjdk-8-jre
+
+git clone https://git.rousse.me/mrousse/SFE-Cloud-Sec.git
+#cd SFE-Cloud-Sec/ODL-Controller
+cd SFE-Cloud-Sec
+mkdir ~/.m2
+wget -q -O - https://raw.githubusercontent.com/opendaylight/odlparent/master/settings.xml > ~/.m2/settings.xml
+wget "https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/groupbasedpolicy/l2-l3-domain-extension/0.4.2-Boron-SR2/l2-l3-domain-extension-0.4.2-Boron-SR2-javadoc.jar"
+mvn install:install-file -DgroupId=org.opendaylight.groupbasedpolicy -DartifactId=l2-l3-domain-extension -Dversion=0.4.2-Boron-SR2 -Dpackaging=bundle -Dfile=l2-l3-domain-extension-0.4.2-Boron-SR2-javadoc.jar
+mvn clean install -DskipTests
+mkdir -p /root/distribution-karaf-0.5.2-Boron-SR2/system/com/orange
+cp -r ~/.m2/repository/com/orange/cloudsec /root/distribution-karaf-0.5.2-Boron-SR2/system/com/orange
---
+++ SFC/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir ovs
+cd ovs
+sudo apt-get install -y git autoconf libtool build-essential initramfs-tools firmware-linux-free irqbalance linux-headers-3.16 python-six python-pip graphviz debhelper dh-autoreconf libssl-dev python-all python-qt4 python-twisted-conch python-zopeinterface dkms uuid-runtime vim
+git clone https://github.com/yyang13/ovs_nsh_patches.git
+git clone https://github.com/openvswitch/ovs.git
+cd ovs
+git reset --hard 7d433ae57ebb90cd68e8fa948a096f619ac4e2d8
+cp ../ovs_nsh_patches/*.patch ./
+git am *.patch
+dpkg-checkbuilddeps 
+DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
+cd ..
+sudo dpkg -i openvswitch-common_2.5.90-1_amd64.deb openvswitch-switch_2.5.90-1_amd64.deb openvswitch-datapath-dkms_2.5.90-1_all.deb 
