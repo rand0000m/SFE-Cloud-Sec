d99c0ed9dddbc6eb3ec06cb2baafbffd8d05492a
Mathieu Rousse <mathieu@rousse.me>
2017-08-10 13:35:31 +0200

    Correction flows SF + Ajout Bridge SFF à la config

--- SFF/config.sh
+++ SFF/config.sh
@@ -2,8 +2,10 @@
 
 if [ -z "$1" ]
 then
-	echo "Usage : $0 <controller-ip>"
+	echo "Usage : $0 <controller-ip> <bridge-name>"
 	exit 1
 fi
 
 ovs-vsctl set-manager tcp:$1:6640
+ovs-vsctl add-br $2
+ovs-vsctl set-controller $2 tcp:$1:6653
